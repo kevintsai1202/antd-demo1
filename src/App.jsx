9bb905444652c228afe42d7b29d1aef14a944f20
kevintsai1202 <kevintsai1202@gmail.com>
2024-03-31 10:14:31 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import { Input, Button } from 'antd';
 import { ProTable } from '@ant-design/pro-components';
 
 const API_URL = 'https://jsonplaceholder.typicode.com/posts';
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  const actionRef = useRef();
+
+  const fetchSomeData = () => {
+    actionRef?.current?.reload();
+  };
 
   const fetchData = async () => {
     setLoading(true);
     try {
-      const response = await fetch(API_URL,  {
+      const response = await fetch(API_URL+`/${abc}`,  {
         headers:{"Content-Type": "application/json"}
       })
       const json = await response.json()
       const newDatas = {
         data:json,
         success:true,
-        total:100
+        total:json.length
       }
       return Promise.resolve(newDatas);
     } catch (error) {
@@ -45,14 +51,18 @@ const App = () => {
   ];
 
   return (
+    <>
+    請輸入查詢內容:<Input></Input> <Button>查詢</Button>
     <ProTable
+      actionRef={actionRef}
       columns={columns}
       request={fetchData}
       loading={loading}
       rowKey="id"
-      search={false}
       toolBarRender={false}
+      search={false}
     />
+    </>
   );
 };
 
